0b618b1eb6e347713c19dcf6bd47d478dfab6b05
trueProger <vovasollo@yandex.ru>
2017-12-15 17:02:26 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use( (req, res, next) => {
+   let now = new Date().toString();
+   let log = `${now} -- url: ${req.url}`;
+   fs.appendFileSync('server.log', log + '\n');
+   next();
+});
+
+
+// app.use( (req, res, next) => {
+//    res.render('maintenance.hbs');
+// });
+
+app.use(express.static( __dirname + '/public'));
+
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        pageTitle: 'main',
+        hello: 'This is my first NODE JS dynamic web page!!! Yes Yes Yes!!!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'about',
+        body: 'fuck PHP!!'
+    });
+});
+
+
+app.listen(8080, () => {
+    console.log('server up');
+});
